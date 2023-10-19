363e75a802aa760596c9febc99a8ee1df4bfbc12
Your Name <you@example.com>
2023-10-18 21:36:37 -0300

    Creando SvBilletera

---
+++ src/main/java/com/mycompany/apuestatodook/SvBilletera.java
@@ -0,0 +1,27 @@
+package com.mycompany.apuestatodook;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "SvBilletera", urlPatterns = "/Billetera")
+
+public class SvBilletera extends HttpServlet{
+    
+   
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp){
+        
+    }
+    
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        
+    }
+    
+}
