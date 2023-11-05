a127b7d258d96ac2963150f46a7317825177295a
Elvira <you@example.com>
2023-11-05 19:16:38 -0300

    Metodo getDineroPorIdUsuario

--- src/main/java/com/mycompany/apuestatodook/model/UsuarioDAO.java
+++ src/main/java/com/mycompany/apuestatodook/model/UsuarioDAO.java
@@ -75,6 +75,32 @@ public class UsuarioDAO {
             throw new RuntimeException(ex);
         }
     }
+    
+    
+    public double getDineroPorIdUsuario (Integer Id) {
+        
+        String query = "SELECT dinero FROM usuario WHERE id_usuario = ?";
+        
+        double dinero = 0.0;
+        
+        try (Connection con = ConnectionPool.getInstance().getConnection(); PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            
+            preparedStatement.setInt(1, Id);
+            
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                
+                if (resultSet.next()) { 
+                    dinero = resultSet.getDouble(4);
+                }  
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        
+        return dinero;
+    }
+
+    
 }
     
 
