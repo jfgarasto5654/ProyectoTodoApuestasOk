8945f2b438ac959f0900f33176fb3c587e069aaf
Elvira <you@example.com>
2023-11-05 18:21:53 -0300

    Metodo doGet de BilleteraServlet

--- src/main/java/com/mycompany/apuestatodook/BilleteraServlet.java
+++ src/main/java/com/mycompany/apuestatodook/BilleteraServlet.java
@@ -1,5 +1,7 @@
 package com.mycompany.apuestatodook;
 
+import com.mycompany.apuestatodook.model.Usuario;
+import com.mycompany.apuestatodook.model.UsuarioDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,15 +10,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-@WebServlet(name = "SvBilletera", urlPatterns = "/billetera")
+@WebServlet(name = "SvBilletera", urlPatterns = "/Billetera")
 
 public class BilleteraServlet extends HttpServlet{
     
    
     @Override
-    protected void doGet (HttpServletRequest req, HttpServletResponse resp){
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        String destino;
+        
+        Usuario usuario = (Usuario)request.getSession().getAttribute("usuario"); 
+        
+        int IDusuario = usuario.getIDusuario();
+
+        double dineroDisponible = usuarioDAO.getDineroPorIdUsuario(IDusuario);
+        
+        request.setAttribute("dinero", dineroDisponible );
+        
+        destino = "WEB-INF/jsp/billetera.jsp";
+        
+        request.getRequestDispatcher(destino).forward(request, response);
     }
+
+
     
     @Override
     protected void doPost (HttpServletRequest req, HttpServletResponse resp)
